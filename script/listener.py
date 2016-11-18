a4bec9047d97451c1da114a102ac9f98ea360e82
Nick <nicholas.reuter2013@gmail.com>
2016-11-17 23:59:08 -0500

    Adds description to python listener script

--- script/listener.py
+++ script/listener.py
@@ -1,3 +1,7 @@
+########################
+# Script to debug Band Reader
+# Listens for anything coming in from the serial
+########################
 import serial
 
 ser = serial.Serial("/dev/ttyUSB0", 9600)
